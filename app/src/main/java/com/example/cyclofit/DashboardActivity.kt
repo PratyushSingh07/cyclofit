2b817d579f1ab6ffa9d0cd5296c9d1504dd64d83
Pratyush Singh <aries.pratyush@gmail.com>
2022-11-17 13:18:03 +0530

    First commit

---
+++ app/src/main/java/com/example/cyclofit/DashboardActivity.kt
@@ -0,0 +1,74 @@
+package com.example.cyclofit
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
+
+class DashboardActivity:AppCompatActivity() {
+    private var mCount:Int=-1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        supportActionBar?.hide()
+
+        val homeFragment=HomeFragment()
+        val healthFragment=HealthFragment()
+        val communityFragment=CommunityFragment()
+        val leaderboardFragment=LeaderboardFragment()
+
+        setCurrentFragment(homeFragment)
+
+        val myBottomNavigationView = findViewById<ChipNavigationBar>(R.id.nav_view)
+        myBottomNavigationView.setItemSelected(R.id.home)
+        myBottomNavigationView.setOnItemSelectedListener { item ->
+            when (item) {
+                R.id.home -> {
+                    mCount=1
+                    Log.i("NavBar", "Home pressed")
+                    setCurrentFragment(homeFragment)
+                }
+                R.id.community -> {
+                    mCount=0
+                    Log.i("NavBar", "Profile pressed")
+                    setCurrentFragment(communityFragment)
+                }
+                R.id.health -> {
+                    mCount=0
+                    setCurrentFragment(healthFragment)
+                }
+                R.id.leaderboard -> {
+                    mCount=0
+                    setCurrentFragment(leaderboardFragment)
+                }
+                else -> {
+                    Log.i("NavBar", "Error?")
+
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if(mCount==0)
+        goHomeFragment()
+        else
+        super.onBackPressed()
+        mCount++
+    }
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.nav_host_fragment_activity_dashboard, fragment)
+            commit()
+        }
+    private fun goHomeFragment(){
+        val fragmentHome = HomeFragment()
+        findViewById<ChipNavigationBar>(R.id.nav_view)?.setItemSelected(R.id.home)
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.nav_host_fragment_activity_dashboard,fragmentHome)
+            commit()
+        }
+    }
+}
