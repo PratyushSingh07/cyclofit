a9c6f4835d8fdf1d70af95946602ca8af7034937
Pratyush Singh <aries.pratyush@gmail.com>
2022-11-21 18:48:09 +0530

    Post fragment updated

--- app/src/main/java/com/example/cyclofit/ui/fragment/PostFragment.kt
+++ app/src/main/java/com/example/cyclofit/ui/fragment/PostFragment.kt
@@ -1,24 +1,50 @@
 package com.example.cyclofit.ui.fragment
 
+import android.R
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.database.Cursor
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.cyclofit.R
-import com.example.cyclofit.databinding.FragmentHomeBinding
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.cyclofit.databinding.FragmentPostBinding
+
 
 class PostFragment : Fragment() {
 
     lateinit var binding: FragmentPostBinding
+    private val RESULT_LOAD_IMAGE = 1
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding=FragmentPostBinding.inflate(inflater,container,false)
-        activity?.window!!.statusBarColor = requireActivity().getColor(R.color.dark_green)
-
+        activity?.window!!.statusBarColor = requireActivity().getColor(R.color.darker_gray)
+        binding.postImage.setOnClickListener {
+            val i = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(i, RESULT_LOAD_IMAGE)
+        }
         return binding.root
     }
+      override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            val selectedImage = data.data
+            val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+            val cursor: Cursor? =
+                activity?.contentResolver?.query(selectedImage!!, filePathColumn, null, null, null)
+            cursor?.moveToFirst()
+            val columnIndex = cursor?.getColumnIndex(filePathColumn[0])
+            val picturePath = cursor?.getString(columnIndex!!)
+            cursor?.close()
+            binding.postImage.setImageBitmap(BitmapFactory.decodeFile(picturePath))
+        }
+    }
+
 }
