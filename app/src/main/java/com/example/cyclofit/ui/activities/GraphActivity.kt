b2864cfa1cdf30cb84e043ed95aeaa4eead3c09a
Pratyush Singh <aries.pratyush@gmail.com>
2022-11-25 22:14:17 +0530

    Added line graph and timer view

--- app/src/main/java/com/example/cyclofit/ui/activities/GraphActivity.kt
+++ app/src/main/java/com/example/cyclofit/ui/activities/GraphActivity.kt
@@ -1,44 +1,59 @@
 package com.example.cyclofit.ui.activities
 
-import android.content.res.Resources
+
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.cyclofit.R
-import com.example.cyclofit.ui.fragment.HealthFragment
 import com.example.cyclofit.ui.fragment.HealthFragment.Companion.Datalist
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.LimitLine
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+
 
 class GraphActivity :AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_graph)
+        setContentView(com.example.cyclofit.R.layout.activity_graph)
         supportActionBar?.hide()
-        val hf=HealthFragment()
-        if(Datalist.isEmpty()) println("List is empty") else println("List is not empty")
-        for(i in Datalist)println("Hello $i")
-        val barArrayList=mutableListOf<BarEntry>()
-//        barArrayList.add(BarEntry(1f,10f))
-//        barArrayList.add(BarEntry(2f,20f))
-//        barArrayList.add(BarEntry(3f,30f))
-//        barArrayList.add(BarEntry(4f,40f))
+//        val hf=HealthFragment()
+//        if(Datalist.isEmpty()) println("List is empty") else println("List is not empty")
+//        for(i in Datalist)println("Hello $i")
+//        val barArrayList=mutableListOf<BarEntry>()
+////        barArrayList.add(BarEntry(1f,10f))
+////        barArrayList.add(BarEntry(2f,20f))
+////        barArrayList.add(BarEntry(3f,30f))
+////        barArrayList.add(BarEntry(4f,40f))
+//        var x:Int=0
+//        for(i in Datalist){
+//            barArrayList.add(BarEntry(x*1f,i.toInt()*1f))
+//            x++
+//        }
+//        val barChart=findViewById<BarChart>(R.id.barChart)
+//        val barDataSet: BarDataSet = BarDataSet(barArrayList,"My Graph")
+//        val baraData: BarData = BarData(barDataSet)
+//        barChart.data=baraData
+//        barDataSet.setColor(resources.getColor(R.color.purple_200))
+//        barDataSet.valueTextColor= Color.BLACK
+//        barDataSet.valueTextSize=16f
+//        barChart.description.isEnabled=true
+
+        val barArrayList=mutableListOf<Entry>()
         var x:Int=0
         for(i in Datalist){
             barArrayList.add(BarEntry(x*1f,i.toInt()*1f))
             x++
         }
-        val barChart=findViewById<BarChart>(R.id.barChart)
-        val barDataSet: BarDataSet = BarDataSet(barArrayList,"My Graph")
-        val baraData: BarData = BarData(barDataSet)
-        barChart.data=baraData
-        barDataSet.setColor(resources.getColor(R.color.purple_200))
-        barDataSet.valueTextColor= Color.BLACK
-        barDataSet.valueTextSize=16f
-        barChart.description.isEnabled=true
-
+        val lineChart=findViewById<LineChart>(com.example.cyclofit.R.id.reportingChart)
+        val lineDataSet=LineDataSet(barArrayList,"My Graph")
+        val lineData=LineData(lineDataSet)
+        lineChart.data=lineData
+        lineDataSet.setColor(resources.getColor(com.example.cyclofit.R.color.purple_200))
+        lineDataSet.valueTextColor= Color.BLACK
+        lineDataSet.valueTextSize=16f
+        lineChart.description.isEnabled=true
     }
 }
--- app/src/main/java/com/example/cyclofit/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/cyclofit/ui/fragment/HomeFragment.kt
@@ -7,11 +7,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.cyclofit.R
 import androidx.navigation.Navigation.findNavController
+import com.androchef.happytimer.countdowntimer.CircularCountDownView
+import com.androchef.happytimer.countdowntimer.HappyTimer
 import com.example.cyclofit.databinding.FragmentHomeBinding
 import com.example.cyclofit.ui.activities.SettingsActivity
 import com.example.cyclofit.ui.utils.Constants
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : BaseFragment() {
@@ -35,6 +41,68 @@ class HomeFragment : BaseFragment() {
             startActivity(Intent(context,SettingsActivity::class.java))
         }
 
+        binding.circularCountDownView.isTimerTextShown= true
+        binding.circularCountDownView.timerType = HappyTimer.Type.COUNT_DOWN
+        binding.circularCountDownView.timerTextFormat = CircularCountDownView.TextFormat.HOUR_MINUTE_SECOND
+        binding.circularCountDownView.strokeThicknessForeground = 10f
+        binding.circularCountDownView.strokeThicknessBackground = 10f
+        binding.circularCountDownView.strokeColorBackground = ContextCompat.getColor(requireActivity(), R.color.purple_200)
+        binding.circularCountDownView.strokeColorForeground = ContextCompat.getColor(requireActivity(), R.color.purple_200)
+        binding.circularCountDownView.timerTextColor = ContextCompat.getColor(requireActivity(), R.color.purple_200)
+        binding.circularCountDownView.timerTextIsBold = true
+        binding.circularCountDownView.timerTextSize = 13f //this will automatically converted to sp value.
+
+        //Initialize Your Timer with seconds
+        binding.circularCountDownView.initTimer(60)
+
+        //set OnTickListener for getting updates on time. [Optional]
+        binding.circularCountDownView.setOnTickListener(object : HappyTimer.OnTickListener {
+//
+//            //OnTick
+            override fun onTick(completedSeconds: Int, remainingSeconds: Int) {
+//
+            }
+//
+//            //OnTimeUp
+            override fun onTimeUp() {
+
+            }
+        })
+
+//        set OnStateChangeListener [RUNNING, FINISHED, PAUSED, RESUMED, UNKNOWN, RESET, STOPPED] [Optional]
+        binding.circularCountDownView.setStateChangeListener(object : HappyTimer.OnStateChangeListener {
+            override fun onStateChange(
+                state: HappyTimer.State,
+                completedSeconds: Int,
+                remainingSeconds: Int
+            ) {
+
+            }
+        })
+
+        //Call these functions to perform actions
+        //Start Timer
+        binding.circularCountDownView.setOnClickListener{
+
+            binding.circularCountDownView.startTimer()
+        }
+
+        //Pause Timer
+        binding.circularCountDownView.pauseTimer()
+
+        //Resume Timer
+//        circularCountDownView.resumeTimer()
+//
+//        //Stop Timer
+//        circularCountDownView.stopTimer()
+//
+//        //Reset Timer
+//        circularCountDownView.resetTimer()
+//
+//        //get Total Seconds
+//        val totalSeconds = circularCountDownView.getTotalSeconds()
+
         return binding.root
+
     }
 }
